8bbcc9496ff392aaa4f2a74bfab8b1aa395e7a89
Janice Taylor <notjanicetaylor@gmail.com>
2022-01-02 22:52:47 -0600

    initial commit

---
+++ bibleverse/bibleverse/bibleverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  bibleverseApp.swift
+//  bibleverse
+//
+//  Created by Janice on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct bibleverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
