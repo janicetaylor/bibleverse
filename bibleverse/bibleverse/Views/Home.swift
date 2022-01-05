d5b0bcc6f008fcfee714bf51b44983b3d2a79cc4
Janice Taylor <notjanicetaylor@gmail.com>
2022-01-04 20:37:42 -0600

    needs work

--- bibleverse/bibleverse/Views/Home.swift
+++ bibleverse/bibleverse/Views/Home.swift
@@ -14,29 +14,40 @@ struct Home: View {
     
     func updateViewIfToday() async {
         
-        // needs help!
-        await loadData(url: URL(string: randomVerseUrl)!)
+        // never opened app before
+        if(UserDefaults.standard.object(forKey: "todayDate") == nil) {
+            UserDefaults.standard.set(Date(), forKey: "todayDate")
+            await loadData(url: URL(string: randomVerseUrl)!)
+            print("xxx \(randomVerse)")
+            return
+        }
         
+        // is today the same day as the last time we opened app?
+        let olderDate = UserDefaults.standard.object(forKey: "todayDate") as! Date
+        let now = Date()
+        let isSameDate = Calendar.current.isDate(now, inSameDayAs: olderDate)
+
+        if(isSameDate) {
+            // already loaded, don't do anything
+            print("xxx isSameDate \(randomVerse) \(randomVerse.count)")
+            // if object is empty for some reason 
+            if(randomVerse.count == 0) {
+                await loadData(url: URL(string: randomVerseUrl)!)
+                return
+            }
+            let data = UserDefaults.standard.data(forKey: "decodedResponse")
+            do {
+                randomVerse = try (NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data!) as? [RandomResult])!
+            } catch {
+               print("ERROR: \(error.localizedDescription)")
+               print("can't get random verse from user defaults")
+               randomVerse = [RandomResult(bookname: "Deuteronomy", chapter: "13", verse: "14", text: "with the harvest produced by the daylight and by the moonlight;")]
+            }
+        }
+        else {
+            await loadData(url: URL(string: randomVerseUrl)!)
+        }
         
-//        let calendar = Calendar.current
-//        var dateToCheck = Date()
-//
-//        if(UserDefaults.standard.object(forKey: "todayDate") == nil) {
-//            dateToCheck = Date()
-//            await loadData(url: URL(string: randomVerseUrl)!)
-//            UserDefaults.standard.set(dateToCheck, forKey: "todayDate")
-//        }
-//        else {
-//            dateToCheck = UserDefaults.standard.object(forKey: "todayDate") as! Date
-//            if(!calendar.isDateInToday(dateToCheck)) {
-//                await loadData(url: URL(string: randomVerseUrl)!)
-//                UserDefaults.standard.set(dateToCheck, forKey: "todayDate")
-//            }
-//            else {
-//                let decoded = UserDefaults.standard.data(forKey: "todayVerse")
-//                randomVerse = NSKeyedUnarchiver.unarchiveObject(with: decoded!) as! [RandomResult]
-//            }
-//        }
     }
     
     func loadData(url:URL) async {
@@ -48,6 +59,9 @@ struct Home: View {
             if let decodedResponse = try? JSONDecoder().decode([RandomResult].self, from: data) {
                     print(decodedResponse)
                     randomVerse = decodedResponse
+                
+                    let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: decodedResponse, requiringSecureCoding: false)
+                    UserDefaults.standard.set(encodedData, forKey: "decodedResponse")
                 }
         } catch {
             print("Invalid verse data")
