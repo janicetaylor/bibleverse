c2e64d9d03473a96ab72c6b062be92e617d3fcdf
Janice Taylor <notjanicetaylor@gmail.com>
2022-01-04 22:04:20 -0600

    user defaults finally working

--- bibleverse/bibleverse/Views/Home.swift
+++ bibleverse/bibleverse/Views/Home.swift
@@ -12,13 +12,15 @@ struct Home: View {
     @State var randomVerse = [RandomResult]()
     var randomVerseUrl = "https://labs.bible.org/api/?passage=random&type=json"
     
+    let defaultVerse = [RandomResult(bookname: "Deuteronomy", chapter: "13", verse: "14", text: "with the harvest produced by the daylight and by the moonlight;")]
+    let userDefaultsVerseKey = "randomverse"
+    
     func updateViewIfToday() async {
         
         // never opened app before
         if(UserDefaults.standard.object(forKey: "todayDate") == nil) {
             UserDefaults.standard.set(Date(), forKey: "todayDate")
             await loadData(url: URL(string: randomVerseUrl)!)
-            print("xxx \(randomVerse)")
             return
         }
         
@@ -26,46 +28,70 @@ struct Home: View {
         let olderDate = UserDefaults.standard.object(forKey: "todayDate") as! Date
         let now = Date()
         let isSameDate = Calendar.current.isDate(now, inSameDayAs: olderDate)
-
+        
         if(isSameDate) {
             // already loaded, don't do anything
-            print("xxx isSameDate \(randomVerse) \(randomVerse.count)")
-            // if object is empty for some reason 
+            // if object is empty for some reason
             if(randomVerse.count == 0) {
-                await loadData(url: URL(string: randomVerseUrl)!)
-                return
-            }
-            let data = UserDefaults.standard.data(forKey: "decodedResponse")
-            do {
-                randomVerse = try (NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data!) as? [RandomResult])!
-            } catch {
-               print("ERROR: \(error.localizedDescription)")
-               print("can't get random verse from user defaults")
-               randomVerse = [RandomResult(bookname: "Deuteronomy", chapter: "13", verse: "14", text: "with the harvest produced by the daylight and by the moonlight;")]
+                if(UserDefaults.standard.object(forKey: userDefaultsVerseKey) == nil) {
+                    await loadData(url: URL(string: randomVerseUrl)!)
+                    return
+                }
+                else {
+                    randomVerse = getVerse()
+                }
             }
         }
         else {
             await loadData(url: URL(string: randomVerseUrl)!)
         }
-        
     }
     
     func loadData(url:URL) async {
         do {
-            print("task succeeded")
             let (data, _) = try await URLSession.shared.data(from: url)
-            print(data)
-            print(url)
             if let decodedResponse = try? JSONDecoder().decode([RandomResult].self, from: data) {
                     print(decodedResponse)
                     randomVerse = decodedResponse
-                
-                    let encodedData = try? NSKeyedArchiver.archivedData(withRootObject: decodedResponse, requiringSecureCoding: false)
-                    UserDefaults.standard.set(encodedData, forKey: "decodedResponse")
+                    
+                    let verseToSave = [RandomResult(bookname: decodedResponse[0].bookname, chapter: decodedResponse[0].chapter, verse: decodedResponse[0].verse, text: decodedResponse[0].text)]
+                   
+                    saveVerse(verse: verseToSave)
                 }
         } catch {
             print("Invalid verse data")
         }
+    }
+    
+    func saveVerse(verse:[RandomResult]) {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(verse)
+            UserDefaults.standard.set(data, forKey: userDefaultsVerseKey)
+            UserDefaults.standard.synchronize()
+            
+            let verse = getVerse()
+        }
+        catch {
+            print("unable to encode verse (\(error.localizedDescription))")
+        }
+    }
+    
+    func getVerse() -> [RandomResult] {
+        var verse = defaultVerse
+        
+        if let data = UserDefaults.standard.data(forKey: userDefaultsVerseKey) {
+            do {
+                let decoder = JSONDecoder()
+                let verseDecoder = try decoder.decode([RandomResult].self, from: data)
+                verse = verseDecoder
+            }
+            catch {
+                print("unable to decode verse (\(error.localizedDescription))")
+            }
+        }
+        
+        return verse
     }
     
     var body: some View {
